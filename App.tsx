f9e74444cee24e93a918b3ac4555329df2810034
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-12-04 22:54:05 -0300

    Implement TaskPod

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import {
 
 } from 'react-native'
 import Counter from 'src/components/Counter'
+import TaskPod from 'src/components/TaskPod'
 
 class App extends React.Component {
 
 	render() {
+		const counter = <Counter/>
 		return (
-			<Counter/>
+			<TaskPod counter={<Counter/>}/>
 		)
 	}
 
---
+++ src/components/TaskPod.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+	View,
+	TextInput,
+	StyleSheet
+} from 'react-native'
+
+interface Props {
+	counter : Element
+}
+
+export default class TaskPod extends React.Component<Props> {
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<TextInput style={styles.txtTitle}>Game</TextInput>
+				{this.props.counter}
+			</View>
+		)
+	}
+
+}
+
+const styles = StyleSheet.create({
+	container : {
+		flexDirection : 'row',
+		alignItems : 'center',
+		justifyContent : 'space-between',
+		backgroundColor : 'gray',
+		padding : 5,
+		borderRadius : 5
+	},
+	txtTitle : {
+		fontSize : 20
+	}
+})

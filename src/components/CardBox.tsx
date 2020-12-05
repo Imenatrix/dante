59daf9db15776635af88c5ac05ffd82d84ad0010
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-12-05 00:27:38 -0300

    Add CardBox header

--- src/components/CardBox.tsx
+++ src/components/CardBox.tsx
@@ -2,27 +2,70 @@ import React from 'react'
 import {
 	ScrollView,
 	Dimensions,
-	StyleSheet
+	StyleSheet,
+	View,
+	Pressable,
+	TextInput
 } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default class CardBox extends React.Component {
 
 	render() {
 		return (
-			<ScrollView
-				horizontal
-				disableIntervalMomentum
-				contentContainerStyle={styles.contentContainerStyle}
-				snapToInterval={Dimensions.get('window').width - 30}
-				decelerationRate='fast'>
-				{this.props.children}
-			</ScrollView>
+			<View style={styles.container}>				
+				<View style={styles.header}>
+					<Pressable style={styles.btnSideMenu}>
+						<Icon style={styles.iconBtnSideMenu} name='menu'/>
+					</Pressable>
+					<TextInput style={styles.txtTitle}>
+						Week
+					</TextInput>
+					<Pressable style={styles.btnConfirm}>
+						<Icon style={styles.iconBtnConfirm} name='check'/>
+					</Pressable>
+				</View>
+				<ScrollView
+					horizontal
+					disableIntervalMomentum
+					contentContainerStyle={styles.contentContainerStyle}
+					snapToInterval={Dimensions.get('window').width - 30}
+					decelerationRate='fast'>
+					{this.props.children}
+				</ScrollView>
+			</View>
 		)
 	}
 
 }
 
 const styles = StyleSheet.create({
+	container : {
+		flex : 1
+	},
+	header : {
+		flexDirection : 'row',
+		alignItems : 'center',
+		backgroundColor : 'gray',
+		justifyContent : 'space-between',
+		padding : 5
+	},
+	btnSideMenu : {
+
+	},
+	iconBtnSideMenu : {
+		fontSize : 40
+	},
+	btnConfirm : {
+		backgroundColor : 'green',
+		borderRadius : 99999
+	},
+	iconBtnConfirm : {
+		fontSize : 40
+	},
+	txtTitle : {
+		fontSize : 25
+	},
 	contentContainerStyle : {
 		paddingLeft : 10,
 		paddingRight : 20

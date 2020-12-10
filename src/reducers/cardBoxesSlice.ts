abffa6ef31da1e1b90da195569b23588b978b7dd
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-12-10 19:48:18 -0300

    Type annotate cardBoxReducer

--- src/reducers/cardBoxesSlice.ts
+++ src/reducers/cardBoxesSlice.ts
@@ -1,20 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+interface Task {
+	id : number,
+	title : string,
+}
+
+interface Card {
+	id : number,
+	title : string,
+	tasks : Task[]
+}
+
+interface CardBox {
+	id : number,
+	title : string,
+	cards : Card[]
+}
+
+const initialState : CardBox[] = [{
+	id : 0,
+	title : 'Week',
+	cards : []
+}]
+
 export const cardBoxesSlice = createSlice({
 	name : 'cardBoxes',
-	initialState : [{
-		id : 0,
-		title : 'Week',
-		cards : []
-	}],
+	initialState : initialState,
 	reducers : {
 		addCardBox : (state) => {
 			const id = state.map(cardBox => cardBox.id).sort().reverse()[0] + 1
-			state.push({
+			const cardBox : CardBox = {
 				id : id,
-				title : 'treco',
+				title : 'Wook',
 				cards : []
-			})
+			}
+			state.push(cardBox)
 		}
 	}
 })

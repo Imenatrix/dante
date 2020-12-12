7e06f6f41cb34195871e3ea8923a397ff2bc2633
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-12-12 00:34:00 -0300

    Make SideMenu opening animation smoother

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -4,7 +4,9 @@ import {
 	View,
 	Animated,
 	Pressable,
-	GestureResponderEvent
+	GestureResponderEvent,
+	Dimensions,
+	Easing
 } from 'react-native'
 
 interface Props {
@@ -19,36 +21,42 @@ interface State {
 	open : boolean
 }
 
+const width = Dimensions.get('window').width
+
 const SideMenu : React.FC<Props> = (props) => {
 
 	const backgroundOpacity = useRef(props.open ? new Animated.Value(0.4) : new Animated.Value(0)).current
-	const bodyFlex = useRef(props.open ? new Animated.Value(3) : new Animated.Value(0)).current
+	const bodyX = useRef(props.open ? new Animated.Value(0) : new Animated.Value(-(width * 3) / 4)).current
 	const [isOpen, setOpen] = useState(props.open != undefined ? props.open : false)
 
 	function open() {
 		setOpen(true)
-		Animated.timing(bodyFlex, {
-			toValue : 3,
-			duration : 300,
-			useNativeDriver : false
+		Animated.timing(bodyX, {
+			toValue : 0,
+			duration : 500,
+			useNativeDriver : true,
+			easing : Easing.out(Easing.exp)
 		}).start()
 		Animated.timing(backgroundOpacity, {
 			toValue : 0.4,
-			duration : 300,
-			useNativeDriver : false
+			duration : 500,
+			useNativeDriver : true,
+			easing : Easing.out(Easing.exp)
 		}).start()
 	}
 
 	function close() {
-		Animated.timing(bodyFlex, {
-			toValue : 0,
-			duration : 300,
-			useNativeDriver : false
+		Animated.timing(bodyX, {
+			toValue : -(width * 3) / 4,
+			duration : 500,
+			useNativeDriver : true,
+			easing : Easing.out(Easing.exp)
 		}).start()
 		Animated.timing(backgroundOpacity, {
 			toValue : 0,
-			duration : 300,
-			useNativeDriver : false
+			duration : 500,
+			useNativeDriver : true,
+			easing : Easing.out(Easing.exp)
 		}).start(() => setOpen(false))
 	}
 
@@ -66,11 +74,11 @@ const SideMenu : React.FC<Props> = (props) => {
 	return (
 		<View style={styles.container}>
 			{isOpen && <>
-				<Animated.View style={[styles.body, {flex : bodyFlex}]}>
-					{props.children}
-				</Animated.View>
 				<Animated.View style={[styles.background, {opacity : backgroundOpacity}]}>
 					<Pressable onPress={onBackgroundPress} style={styles.backgroundPressable}/>
+				</Animated.View>
+				<Animated.View style={[styles.body, {transform : [{translateX : bodyX}]}]}>
+					{props.children}
 				</Animated.View>
 			</>}
 		</View>
@@ -88,6 +96,9 @@ const styles = StyleSheet.create({
 		flexDirection : 'row'
 	},
 	body : {
+		position : 'absolute',
+		width : '75%',
+		height : '100%',
 		backgroundColor : 'white'
 	},
 	background : {

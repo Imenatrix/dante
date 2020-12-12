fc2bdfebffa3b3009806e2ec737e846d28d85422
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-12-12 15:51:13 -0300

    Add go mode and edit mode to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,7 @@ import {
 	StyleSheet,
 	View,
 	Dimensions,
+	Pressable
 } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from 'src/reducers'
@@ -12,6 +13,7 @@ import NewPod from 'src/components/NewPod'
 import { Card as ICard } from 'src/reducers/cardsSlice'
 import { add } from 'src/reducers/tasksSlice'
 import CardHeader from 'src/components/CardHeader'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 interface Props {
 	card : ICard
@@ -22,6 +24,7 @@ const Card : React.FC<Props> = (props) => {
 	const dispatch = useDispatch()
 
 	const card = props.card
+	const mode = useSelector((state : RootState) => state.mode.value)
 	const tasks = useSelector((state : RootState) => state.tasks).filter(task => task.cardId == card.id)
 
 	return (
@@ -31,8 +34,15 @@ const Card : React.FC<Props> = (props) => {
 				{tasks.map((task) => (
 					<TaskPod key={task.id} task={task}/>
 				))}
-				<NewPod onPress={() => dispatch(add({cardId : card.id}))}/>
+				{mode === 'edit' &&
+					<NewPod onPress={() => dispatch(add({cardId : card.id}))}/>
+				}
 			</ScrollView>
+			{mode === 'go' &&
+				<Pressable style={styles.btnGo}>
+					<Icon style={styles.iconBtnGo} name='play-arrow'/>
+				</Pressable>
+			}
 		</View>
 	)
 
@@ -52,5 +62,14 @@ const styles = StyleSheet.create({
 	podDrawer : {
 		paddingVertical : 5,
 		backgroundColor : 'lightgray'
+	},
+	btnGo : {
+		height : 60,
+		backgroundColor : 'green',
+		justifyContent : 'center',
+		alignItems : 'center'
+	},
+	iconBtnGo : {
+		fontSize : 50
 	}
 })

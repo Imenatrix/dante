e1a1aa2e5ea5c61427dbfcca670c0beab8af983d
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-12-12 15:01:33 -0300

    Implement redux-persist

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React, {} from 'react'
 import {Provider} from 'react-redux'
-import store from 'src/store'
+import { store, persistor } from 'src/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import Main from 'src/components/Main'
 
 const App : React.FC = () => {
 	return (
 		<Provider store={store}>
-			<Main/>
+			<PersistGate loading={null} persistor={persistor}>
+				<Main/>
+			</PersistGate>
 		</Provider>
 	)
 }
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage'
 import rootReducer from 'src/reducers'
 
-export default configureStore({
-	reducer : rootReducer
-})
+const persistConfig = {
+	key : 'root',
+	storage : AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store)

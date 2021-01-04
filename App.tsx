d44e023ea60b7326532faca5f245522bd2c5e978
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2021-01-03 21:13:52 -0300

    Make screen always on while dante is running

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import {Provider} from 'react-redux'
 import { store, persistor } from 'src/store'
 import { PersistGate } from 'redux-persist/integration/react'
 import Main from 'src/components/Main'
+import { useKeepAwake } from '@sayem314/react-native-keep-awake'
 
 const App : React.FC = () => {
+	useKeepAwake()
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>

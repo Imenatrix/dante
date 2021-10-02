4e1e216ef696fb516b3b0895aa5c16a98ec9e131
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-10-02 16:55:00 -0300

    Add drop shadow to NewPod

--- src/components/NewPod.tsx
+++ src/components/NewPod.tsx
@@ -4,6 +4,7 @@ import {
 	Pressable,
 	StyleSheet
 } from 'react-native'
+import DropShadow from 'react-native-drop-shadow'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 interface Props {
@@ -15,9 +16,11 @@ const NewPod : React.FC<Props> = (props) => {
 	const onPress = props.onPress
 
 	return (
-		<Pressable onPress={onPress} style={styles.container}>
-			<Icon style={styles.icon} name='add'/>
-		</Pressable>
+		<DropShadow style={styles.shadow}>
+			<Pressable onPress={onPress} style={styles.container}>
+				<Icon style={styles.icon} name='add'/>
+			</Pressable>
+		</DropShadow>
 	)
 
 }
@@ -25,16 +28,21 @@ const NewPod : React.FC<Props> = (props) => {
 export default NewPod
 
 const styles = StyleSheet.create({
+	shadow : {
+		shadowOpacity : 0.3,
+		shadowRadius : 1,
+		shadowColor : 'black'
+	},
 	container : {
 		height : 60,
 		margin : 5,
 		justifyContent : 'center',
 		alignItems : 'center',
-		backgroundColor : 'gainsboro',
-		borderRadius : 5
+		borderRadius : 5,
+		backgroundColor : 'white'
 	},
 	icon : {
 		fontSize : 50,
-		color : 'white'
+		color : 'lightgray'
 	}
 })
